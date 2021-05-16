9118dbdc83e01ff96daad0bdb79390b449b76470
mika1407 <koneautomaatio@hotmail.com>
2021-05-16 21:07:25 +0300

    valokuva galleria

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import images from './data';
+import { SRLWrapper } from 'simple-react-lightbox';
+
+const options = {
+	// settings: {
+	// 	overlayColor: 'rgb(25, 136, 124)',
+	// 	autoplaySpeed: 1500,
+	// 	transitionSpeed: 900
+	// },
+	// buttons: {
+	// 	backgroundColor: 'red',
+	// 	iconColor: 'rgba(126, 172, 139, 0.8)'
+	// },
+	// caption: {
+	// 	captionColor: '#a6cfa5',
+	// 	captionFontFamily: 'Raleway, sans-serif',
+	// 	captionFontWeight: '300',
+	// 	captionTextTransform: 'uppercase'
+	// },
+	// progressBar: {
+	// 	height: '20px',
+	// 	fillColor: 'blue',
+	// 	backgroundColor: 'white'
+	// }
+};
+
 
 function App() {
+  const [tag, setTag] = useState('all');
+  const [ filteredImages, setFilteredImages] = useState([]);
+
+  	useEffect(
+		() => {
+			tag === 'all' ? setFilteredImages(images) : setFilteredImages(images.filter(image => image.tag === tag));
+		},
+		[tag]
+	);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" handleSetTag={setTag}>
+      <div className="tags">
+        <TagButton name="all" handleSetTag={setTag} tagActive={tag === 'all' ? true : false}/>
+        <TagButton name="new" handleSetTag={setTag} tagActive={tag === 'new' ? true : false}/>
+        <TagButton name="free" handleSetTag={setTag} tagActive={tag === 'free' ? true : false}/>
+        <TagButton name="pro" handleSetTag={setTag} tagActive={tag === 'pro' ? true : false}/>
+      </div>
+      <SRLWrapper options={options}>
+        <div className="container">
+          {filteredImages.map(image => (
+            <div key={image.id} className="image-card">
+              <a href={`/images/${image.imageName}`} >
+                <img className="image" src={`/images/${image.imageName}`} alt="" />
+              </a>
+          </div>
+          ))}
+        </div>
+      </SRLWrapper>
     </div>
   );
 }
+
+const TagButton = ({ name, handleSetTag, tagActive }) => {
+	return (
+		<button className={ `tag ${ tagActive ? 'active': null}`} onClick={ () => handleSetTag(name)}>
+			{name.toUpperCase()}
+		</button>
+	);
+};
 
 export default App;
